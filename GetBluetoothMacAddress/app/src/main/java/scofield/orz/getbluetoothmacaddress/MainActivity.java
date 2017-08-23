be2a62336e86eb490462ed4513f532ba02f8493c
Scofield Hsu <scofield.hsu@gmail.com>
2017-08-23 14:27:24 +0800

    use reflect method with aidl to get bluetooth mac address

--- GetBluetoothMacAddress/app/src/main/java/scofield/orz/getbluetoothmacaddress/MainActivity.java
+++ GetBluetoothMacAddress/app/src/main/java/scofield/orz/getbluetoothmacaddress/MainActivity.java
@@ -1,15 +1,25 @@
 package scofield.orz.getbluetoothmacaddress;
 
+
 import android.bluetooth.BluetoothAdapter;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.os.IBinder;
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+
+import java.lang.reflect.Method;
+
+import android.bluetooth.IBluetooth;
+import android.bluetooth.IBluetoothManager;
+import android.bluetooth.IBluetoothManagerCallback;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
     Boolean isBluetoothEnabled = false;
     BluetoothAdapter bluetoothAdapter;
     IntentFilter filter = new IntentFilter();
+    String sBTMacAddr;
+    TextView tv;
 
     // BroadcastReceiver to receive 'BluetoothAdapter.ACTION_STATE_CHANGED'
     class BluetoothStateChangedReceiver extends BroadcastReceiver {
@@ -31,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
                 if (bluetoothState == BluetoothAdapter.STATE_ON) {
                     Log.e("scofield", "bluetooth is enabled");
                     isBluetoothEnabled = true;
-                    //getBluetoothMacAddress();
+                    getBluetoothMacAddress();
                 }
             }
         }
@@ -46,6 +58,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        tv = (TextView) findViewById(R.id.sample_text);
+        tv.setTextSize(50.0f);
+        tv.setText(stringFromJNI());
 
         // check if bluetooth hardware feature exists
         pm = getPackageManager();
@@ -64,13 +81,70 @@ public class MainActivity extends AppCompatActivity {
             }
             else {
                 isBluetoothEnabled = true;
-                //getBluetoothMacAddress();
+                getBluetoothMacAddress();
             }
         }
+    }
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+    void getBluetoothMacAddress() {
+        if (!isBluetoothEnabled) {
+            Log.e("scofield", "getBluetoothMacAddress() !enabled");
+            return;
+        }
+
+        if (android.os.Build.VERSION.SDK_INT <= android.os.Build.VERSION_CODES.JELLY_BEAN) {
+            sBTMacAddr = bluetoothAdapter.getAddress();
+            Log.e("scofield", "original bt mac: " + sBTMacAddr);
+
+            tv.setText("Bluetooth MAC address:\n" + sBTMacAddr);
+
+            return;
+        }
+
+        try {
+            Class<?> classServiceManager = Class.forName("android.os.ServiceManager");
+            Method methodGetService = classServiceManager.getDeclaredMethod("getService", String.class);
+            methodGetService.setAccessible(true);
+
+            Object object = new Object();
+            final IBinder iBinderBTManager = (IBinder) methodGetService.invoke(object, "bluetooth_manager");
+            if (iBinderBTManager == null)
+                Log.e("scofield", "iBinderBTManager is null");
+            else {
+                IBluetoothManager iBTManager = IBluetoothManager.Stub.asInterface(iBinderBTManager);
+
+                IBluetoothManagerCallback iBTManagerCallback = new IBluetoothManagerCallback() {
+                    @Override
+                    public IBinder asBinder() { return iBinderBTManager;    }
+
+                    @Override
+                    public void onBluetoothServiceUp(IBluetooth bluetoothService)
+                            throws RemoteException {    }
+
+                    @Override
+                    public void onBluetoothServiceDown() throws RemoteException {   }
+
+                    @Override
+                    public void onBrEdrDown() throws RemoteException {  }
+                };
+
+                IBluetooth iBT = iBTManager.registerAdapter(iBTManagerCallback);
+                if (iBT == null) {
+                    Log.e("scofield", "IBluetooth is null");
+                    return;
+                }
+
+                String btName = iBTManager.getName();
+                sBTMacAddr = iBT.getAddress();
+                Log.e("scofield", "bluetooth Name: " + btName + " MacAddress: "+sBTMacAddr);
+                iBTManager.unregisterAdapter(iBTManagerCallback);
+
+                tv.setText("Bluetooth MAC address:\n" + sBTMacAddr);
+            }
+        }
+        catch (Exception e) {
+            Log.e("scofield", "scofield_exception:"+e+" cause:"+e.getCause());
+        }
     }
 
     /**
